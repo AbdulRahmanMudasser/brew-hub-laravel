136f9c31d0bab5c9fb0861e6fa7d64b61e5a657c
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-05-26 18:22:41 +0500

    footer.php modified, added styling

--- includes/footer.php
+++ includes/footer.php
@@ -1,27 +1,18 @@
 <footer>
-    <div class="footer-col">
+    <div class="footer-section">
         <h4>Bean & Brew</h4>
-        <p>Crafting unforgettable coffee experiences since 2025.</p>
-        <p>© 2025 Bean & Brew. All Rights Reserved.</p>
+        <p>Crafting unforgettable coffee experiences since 2006.</p>
+        <p>&copy; 2025 Bean & Brew. All Rights Reserved.</p>
     </div>
-    <div class="footer-col">
-        <h4>Contact</h4>
-        <p>123 Coffee Lane, Brewtown</p>
-        <p>Email: <a href="mailto:contact@beanandbrew.com"
-                      style="color: inherit; text-decoration: none;">contact@beanandbrew.com</a></p>
-        <p>Phone: <a href="tel:+1234567890" style="color: inherit; text-decoration: none;">+123 456 7890</a></p>
+    <div class="footer-section">
+        <h4>Contact Us</h4>
+        <p><span class="footer-icon">&#9749;</span> 123 Coffee Lane, Bean Town</p>
+        <p><span class="footer-icon">&#9993;</span> <a href="mailto:contact@beanandbrew.com">contact@beanandbrew.com</a></p>
+        <p><span class="footer-icon">&#9742;</span> <a href="tel:+1234567890">+1 234-567-8909</a></p>
     </div>
-    <div class="footer-col">
-        <h4>Connect</h4>
-        <div class="social-links">
-            <a href="#">Facebook</a>
-            <a href="#">Instagram</a>
-            <a href="#">Twitter</a>
-        </div>
-    </div>
-    <div class="footer-col">
+    <div class="footer-section">
         <h4>Stay Updated</h4>
-        <form class="newsletter">
+        <form class="newsletter-form" action="">
             <input type="email" placeholder="Enter your email" required>
             <input type="submit" value="Subscribe">
         </form>
